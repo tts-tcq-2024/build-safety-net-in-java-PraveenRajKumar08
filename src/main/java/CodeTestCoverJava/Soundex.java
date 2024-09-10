6748409c7ad0ececb42b4e42147176c7c614b77a
PraveenRajKumar08 <jpraveenjo@gmail.com>
2024-09-10 18:52:55 +0530

    Update Soundex.java

--- src/main/java/CodeTestCoverJava/Soundex.java
+++ src/main/java/CodeTestCoverJava/Soundex.java
@@ -2,47 +2,47 @@ package CodeTestCoverJava;
 
 public class Soundex {
 
-    public static String generateSoundex(String name) {
-        if (name == null || name.isEmpty()) {
-            return "";
-        }
+	public static String generateSoundex(String name) {
+	    if (name == null || name.isEmpty()) {
+	        return "";
+	    }
 
-        StringBuilder soundex = new StringBuilder();
-        soundex.append(Character.toUpperCase(name.charAt(0)));
-        char prevCode = getSoundexCode(name.charAt(0));
+	    StringBuilder soundex = new StringBuilder();
+	    soundex.append(Character.toUpperCase(name.charAt(0)));
+	    char prevCode = mapCharToSoundexCode(name.charAt(0));
 
-        for (int i = 1; i < name.length() && soundex.length() < 4; i++) {
-            char code = getSoundexCode(name.charAt(i));
-            if (code != '0' && code != prevCode) {
-                soundex.append(code);
-                prevCode = code;
-            }
-        }
+	    generateSoundexForRemainingChars(name, soundex, prevCode);
 
-        while (soundex.length() < 4) {
-            soundex.append('0');
-        }
+	    while (soundex.length() < 4) {
+	        soundex.append('0');
+	    }
 
-        return soundex.toString();
-    }
+	    return soundex.toString();
+	}
 
-    private static char getSoundexCode(char c) {
-        c = Character.toUpperCase(c);
-        switch (c) {
-            case 'B': case 'F': case 'P': case 'V':
-                return '1';
-            case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z':
-                return '2';
-            case 'D': case 'T':
-                return '3';
-            case 'L':
-                return '4';
-            case 'M': case 'N':
-                return '5';
-            case 'R':
-                return '6';
-            default:
-                return '0'; // For A, E, I, O, U, H, W, Y
-        }
-    }
+	private static void generateSoundexForRemainingChars(String name, StringBuilder soundex, char prevCode) {
+	    for (int i = 1; i < name.length() && soundex.length() < 4; i++) {
+	        char code = mapCharToSoundexCode(name.charAt(i));
+	        if (isValidCode(code, prevCode)) {
+	            soundex.append(code);
+	            prevCode = code;
+	        }
+	    }
+	}
+
+	private static boolean isValidCode(char code, char prevCode) {
+	    return code != '0' && code != prevCode;
+	}
+
+	private static char mapCharToSoundexCode(char c) {
+	    c = Character.toUpperCase(c);
+	    if ("BFPV".indexOf(c) >= 0) return '1';
+	    if ("CGJKQSXZ".indexOf(c) >= 0) return '2';
+	    if ("DT".indexOf(c) >= 0) return '3';
+	    if (c == 'L') return '4';
+	    if ("MN".indexOf(c) >= 0) return '5';
+	    if (c == 'R') return '6';
+	    return '0'; // For A, E, I, O, U, H, W, Y
+	}
+    
 }
