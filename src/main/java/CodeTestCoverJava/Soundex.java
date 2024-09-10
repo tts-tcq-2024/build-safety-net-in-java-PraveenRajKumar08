5f0a7954aa34a438c3e6e0bd296cfbaee80899b0
PraveenRajKumar08 <jpraveenjo@gmail.com>
2024-09-10 19:53:35 +0530

    Update Soundex.java

--- src/main/java/CodeTestCoverJava/Soundex.java
+++ src/main/java/CodeTestCoverJava/Soundex.java
@@ -1,59 +1,67 @@
 package CodeTestCoverJava;
 
 public class Soundex {
-private static final Map<Character, Character> soundexMap = new HashMap<>();
-	 
-	 static {
-	        initializeSoundexMap();
+ private static final Map<Character, Character> SOUNDEX_MAP = new HashMap<>();
+
+	    static {
+	        SOUNDEX_MAP.put('B', '1'); SOUNDEX_MAP.put('F', '1'); SOUNDEX_MAP.put('P', '1'); SOUNDEX_MAP.put('V', '1');
+	        SOUNDEX_MAP.put('C', '2'); SOUNDEX_MAP.put('G', '2'); SOUNDEX_MAP.put('J', '2'); SOUNDEX_MAP.put('K', '2');
+	        SOUNDEX_MAP.put('Q', '2'); SOUNDEX_MAP.put('S', '2'); SOUNDEX_MAP.put('X', '2'); SOUNDEX_MAP.put('Z', '2');
+	        SOUNDEX_MAP.put('D', '3'); SOUNDEX_MAP.put('T', '3');
+	        SOUNDEX_MAP.put('L', '4');
+	        SOUNDEX_MAP.put('M', '5'); SOUNDEX_MAP.put('N', '5');
+	        SOUNDEX_MAP.put('R', '6');
 	    }
-	 public static String generateSoundex(String name) {
-	        if (name == null || name.isEmpty()) {
+
+	    public static String generateSoundex(String name) {
+	        if (isInvalidName(name)) {
 	            return "";
 	        }
 
 	        StringBuilder soundex = new StringBuilder();
-	        soundex.append(Character.toUpperCase(name.charAt(0)));
-	        char prevCode = mapCharToSoundexCode(name.charAt(0));
+	        char firstChar = appendFirstChar(name, soundex);
+	        char prevCode = getSoundexCode(firstChar);
 
-	        generateSoundexForRemainingChars(name, soundex, prevCode);
+	        processRemainingChars(name, soundex, prevCode);
 
-	        return padWithZeros(soundex);
+	        return padWithZeros(soundex).toString();
 	    }
 
-	    private static void generateSoundexForRemainingChars(String name, StringBuilder soundex, char prevCode) {
+	    private static boolean isInvalidName(String name) {
+	        return name == null || name.isEmpty();
+	    }
+
+	    private static char appendFirstChar(String name, StringBuilder soundex) {
+	        char firstChar = Character.toUpperCase(name.charAt(0));
+	        soundex.append(firstChar);
+	        return firstChar;
+	    }
+
+	    private static void processRemainingChars(String name, StringBuilder soundex, char prevCode) {
 	        for (int i = 1; i < name.length() && soundex.length() < 4; i++) {
-	            char code = mapCharToSoundexCode(name.charAt(i));
+	            char currentChar = Character.toUpperCase(name.charAt(i));
+	            char code = getSoundexCode(currentChar);
+
 	            if (isValidCode(code, prevCode)) {
 	                soundex.append(code);
-	                prevCode = code; 
+	                prevCode = code;
 	            }
 	        }
-	    }
-
-	    private static String padWithZeros(StringBuilder soundex) {
-	        while (soundex.length() < 4) {
-	            soundex.append('0');
-	        }
-	        return soundex.toString();
-	    }
-
-	    private static char mapCharToSoundexCode(char c) {
-	        return soundexMap.getOrDefault(Character.toUpperCase(c), '0');
 	    }
 
 	    private static boolean isValidCode(char code, char prevCode) {
-	        return code != '0' && code != prevCode; 
+	        return code != '0' && code != prevCode;
 	    }
 
-	    private static void initializeSoundexMap() {
-	        String[] groups = {"BFPV", "CGJKQSXZ", "DT", "L", "MN", "R"};
-	        char[] codes = {'1', '2', '3', '4', '5', '6'};
-
-	        for (int i = 0; i < groups.length; i++) {
-	            for (char c : groups[i].toCharArray()) {
-	                soundexMap.put(c, codes[i]);
-	            }
+	    private static StringBuilder padWithZeros(StringBuilder soundex) {
+	        while (soundex.length() < 4) {
+	            soundex.append('0');
 	        }
+	        return soundex;
+	    }
+
+	    private static char getSoundexCode(char c) {
+	        return SOUNDEX_MAP.getOrDefault(c, '0');
 	    }
 	
     
