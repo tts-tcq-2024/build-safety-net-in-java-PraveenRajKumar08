a5feb78c01ac3e781c78ec29bd4be238ae93a8e4
PraveenRajKumar08 <jpraveenjo@gmail.com>
2024-09-17 15:06:47 +0530

    Update Soundex.java

--- src/main/java/CodeTestCoverJava/Soundex.java
+++ src/main/java/CodeTestCoverJava/Soundex.java
@@ -1,5 +1,10 @@
 package CodeTestCoverJava;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Soundex {
  
 	public static String generateSoundex(String name) {
