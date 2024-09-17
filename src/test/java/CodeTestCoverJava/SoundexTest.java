daa355bd12b5cfbb54bb2ff18d0c3d4b53844f54
PraveenRajKumar08 <jpraveenjo@gmail.com>
2024-09-17 15:35:58 +0530

    Update SoundexTest.java

--- src/test/java/CodeTestCoverJava/SoundexTest.java
+++ src/test/java/CodeTestCoverJava/SoundexTest.java
@@ -6,130 +6,130 @@ import java.util.Map;
 import java.util.List;
 import java.util.Arrays;
 
-public class TcqCleanCodingTest {
+public class SoundexTest {
 
     // Test cases for generateSoundex method
     @Test
     public void testGenerateSoundex_EmptyString() {
-        assertEquals("", TcqCleanCoding.generateSoundex(""));
+        assertEquals("", Soundex.generateSoundex(""));
     }
 
     @Test
     public void testGenerateSoundex_NullString() {
-        assertEquals("", TcqCleanCoding.generateSoundex(null));
+        assertEquals("", Soundex.generateSoundex(null));
     }
 
     @Test
     public void testGenerateSoundex_SingleCharacter() {
-        assertEquals("A000", TcqCleanCoding.generateSoundex("A"));
+        assertEquals("A000", Soundex.generateSoundex("A"));
     }
 
     @Test
     public void testGenerateSoundex_CommonName() {
-        assertEquals("R163", TcqCleanCoding.generateSoundex("Robert"));
+        assertEquals("R163", Soundex.generateSoundex("Robert"));
     }
 
     @Test
     public void testGenerateSoundex_CaseInsensitivity() {
-        assertEquals("R163", TcqCleanCoding.generateSoundex("Robert"));
-        assertEquals("R163", TcqCleanCoding.generateSoundex("robert"));
+        assertEquals("R163", Soundex.generateSoundex("Robert"));
+        assertEquals("R163", Soundex.generateSoundex("robert"));
     }
 
     @Test
     public void testGenerateSoundex_NonLetterCharacters() {
-        assertEquals("R163", TcqCleanCoding.generateSoundex("R0b3rt"));
+        assertEquals("R163", Soundex.generateSoundex("R0b3rt"));
     }
 
     @Test
     public void testGenerateSoundex_MaxLength() {
-        assertEquals("T522", TcqCleanCoding.generateSoundex("Tymczak"));
+        assertEquals("T522", Soundex.generateSoundex("Tymczak"));
     }
 
     @Test
     public void testGenerateSoundex_DuplicateAdjacentCodes() {
-        assertEquals("P236", TcqCleanCoding.generateSoundex("Pfister"));
+        assertEquals("P236", Soundex.generateSoundex("Pfister"));
     }
 
     @Test
     public void testGenerateSoundex_StartsWithVowel() {
-        assertEquals("E460", TcqCleanCoding.generateSoundex("Euler"));
+        assertEquals("E460", Soundex.generateSoundex("Euler"));
     }
 
     @Test
     public void testGenerateSoundex_AllVowels() {
-        assertEquals("A000", TcqCleanCoding.generateSoundex("AEIOU"));
+        assertEquals("A000", Soundex.generateSoundex("AEIOU"));
     }
 
     @Test
     public void testGenerateSoundex_LargeInput() {
-        assertEquals("A452", TcqCleanCoding.generateSoundex("AlphanumericCharacterInputTest"));
+        assertEquals("A452", Soundex.generateSoundex("AlphanumericCharacterInputTest"));
     }
 
     // Test cases for buildSoundex method
     @Test
     public void testBuildSoundex_CommonName() {
-        assertEquals(new StringBuilder("R163").toString(), TcqCleanCoding.buildSoundex("Robert").toString());
+        assertEquals(new StringBuilder("R163").toString(), Soundex.buildSoundex("Robert").toString());
     }
 
     @Test
     public void testBuildSoundex_VowelStart() {
-        assertEquals(new StringBuilder("E460").toString(), TcqCleanCoding.buildSoundex("Euler").toString());
+        assertEquals(new StringBuilder("E460").toString(), Soundex.buildSoundex("Euler").toString());
     }
 
     @Test
     public void testBuildSoundex_SingleChar() {
-        assertEquals(new StringBuilder("A000").toString(), TcqCleanCoding.buildSoundex("A").toString());
+        assertEquals(new StringBuilder("A000").toString(), Soundex.buildSoundex("A").toString());
     }
 
     // Test cases for checkLength method
     @Test
     public void testCheckLength_Valid() {
-        assertTrue(TcqCleanCoding.checkLength(1, 6, 1));
+        assertTrue(Soundex.checkLength(1, 6, 1));
     }
 
     @Test
     public void testCheckLength_Exceeded() {
-        assertFalse(TcqCleanCoding.checkLength(4, 6, 4));
+        assertFalse(Soundex.checkLength(4, 6, 4));
     }
 
     // Test cases for doAppend method
     @Test
     public void testDoAppend_ValidCode() {
-        assertTrue(TcqCleanCoding.doAppend('3', '1'));
+        assertTrue(Soundex.doAppend('3', '1'));
     }
 
     @Test
     public void testDoAppend_InvalidCode() {
-        assertFalse(TcqCleanCoding.doAppend('0', '3')); 
+        assertFalse(Soundex.doAppend('0', '3')); 
     }
 
     @Test
     public void testDoAppend_DuplicateCode() {
-        assertFalse(TcqCleanCoding.doAppend('3', '3')); 
+        assertFalse(Soundex.doAppend('3', '3')); 
     }
 
     // Test cases for getSoundexCode method
     @Test
     public void testGetSoundexCode_ValidChar() {
-        assertEquals('1', TcqCleanCoding.getSoundexCode('B'));
-        assertEquals('2', TcqCleanCoding.getSoundexCode('C'));
-        assertEquals('5', TcqCleanCoding.getSoundexCode('N'));
+        assertEquals('1', Soundex.getSoundexCode('B'));
+        assertEquals('2', Soundex.getSoundexCode('C'));
+        assertEquals('5', Soundex.getSoundexCode('N'));
     }
 
     @Test
     public void testGetSoundexCode_InvalidChar() {
-        assertEquals('0', TcqCleanCoding.getSoundexCode('A')); 
+        assertEquals('0', Soundex.getSoundexCode('A')); 
     }
 
     @Test
     public void testGetSoundexCode_NonAlphabetChar() {
-        assertEquals('0', TcqCleanCoding.getSoundexCode('9')); 
+        assertEquals('0', Soundex.getSoundexCode('9')); 
     }
 
     // Test cases for buildSoundexMap method
     @Test
     public void testBuildSoundexMap_Valid() {
-        Map<Character, Character> soundexMap = TcqCleanCoding.buildSoundexMap();
+        Map<Character, Character> soundexMap = Soundex.buildSoundexMap();
         assertEquals('1', soundexMap.get('B'));
         assertEquals('2', soundexMap.get('C'));
         assertEquals('3', soundexMap.get('D'));
@@ -138,7 +138,7 @@ public class TcqCleanCodingTest {
     // Test cases for populateSoundexMap method
     @Test
     public void testPopulateSoundexMap_Valid() {
-        Map<Character, Character> populatedMap = TcqCleanCoding.populateSoundexMap(Arrays.asList('A', 'E', 'I', 'O', 'U'), '0');
+        Map<Character, Character> populatedMap = Soundex.populateSoundexMap(Arrays.asList('A', 'E', 'I', 'O', 'U'), '0');
         assertEquals('0', populatedMap.get('A'));
         assertEquals('0', populatedMap.get('E'));
         assertEquals('0', populatedMap.get('I'));
@@ -146,39 +146,39 @@ public class TcqCleanCodingTest {
 
     @Test
     public void testPopulateSoundexMap_EmptyList() {
-        Map<Character, Character> populatedMap = TcqCleanCoding.populateSoundexMap(Arrays.asList(), '0');
+        Map<Character, Character> populatedMap = Soundex.populateSoundexMap(Arrays.asList(), '0');
         assertTrue(populatedMap.isEmpty()); 
     }
 
     // Test cases for isEmptyString method
     @Test
     public void testIsEmptyString_Null() {
-        assertTrue(TcqCleanCoding.isEmptyString(null));
+        assertTrue(Soundex.isEmptyString(null));
     }
 
     @Test
     public void testIsEmptyString_Empty() {
-        assertTrue(TcqCleanCoding.isEmptyString(""));
+        assertTrue(Soundex.isEmptyString(""));
     }
 
     @Test
     public void testIsEmptyString_NonEmpty() {
-        assertFalse(TcqCleanCoding.isEmptyString("A"));
+        assertFalse(Soundex.isEmptyString("A"));
     }
 
     // Test cases for isEmptyList method
     @Test
     public void testIsEmptyList_Null() {
-        assertFalse(TcqCleanCoding.isEmptyList(null)); 
+        assertFalse(Soundex.isEmptyList(null)); 
     }
 
     @Test
     public void testIsEmptyList_Empty() {
-        assertFalse(TcqCleanCoding.isEmptyList(Arrays.asList())); 
+        assertFalse(Soundex.isEmptyList(Arrays.asList())); 
     }
 
     @Test
     public void testIsEmptyList_NonEmpty() {
-        assertTrue(TcqCleanCoding.isEmptyList(Arrays.asList('A', 'B')));
+        assertTrue(Soundex.isEmptyList(Arrays.asList('A', 'B')));
     }
 }
