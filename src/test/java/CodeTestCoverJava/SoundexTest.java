500a6771b38d51e8d9623d2399677f2bfddc5230
PraveenRajKumar08 <jpraveenjo@gmail.com>
2024-09-18 14:56:05 +0530

    Update SoundexTest.java

--- src/test/java/CodeTestCoverJava/SoundexTest.java
+++ src/test/java/CodeTestCoverJava/SoundexTest.java
@@ -1,6 +1,8 @@
 package CodeTestCoverJava;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
